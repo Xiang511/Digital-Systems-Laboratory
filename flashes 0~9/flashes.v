2a118d733fffa8521882849500f45cc8fbbf847a
詳 <toby91511@gmail.com>
2024-04-10 21:40:16 +0800

    Feat : Flash 0~9
    Design and implement a circuit that successively flashes digits 0	through 9 on the 7-segment display HEX0.

---
+++ flashes 0~9/flashes.v
@@ -0,0 +1,24 @@
+module	Lab6_part2	(CLOCK_50,	HEX0);
+    input	CLOCK_50;
+    output	[0:6]	HEX0;
+    wire	[3:0]	bcd;
+    parameter	m	=	25;
+    reg [m-1:0]	slow_count;
+    reg[3:0]	digit_flipper;
+    //	Create	a	1Hz	4-bit	counter
+    //	A	large	counter	to	produce	a	1	second	(approx)	enable	from	the	50	MHz	Clock
+    always	@(posedge CLOCK_50)
+        slow_count <=	slow_count +	1'b1;
+    //	four-bit	counter	that	uses	a	slow	enable	for	selecting	digit
+
+    always	@	(posedge CLOCK_50)
+        if	(slow_count ==	0)
+            if	(digit_flipper ==	4'h9)
+                digit_flipper <=	4'h0;
+            else
+                digit_flipper <=	digit_flipper +	1'b1;
+                
+    assign	bcd =	digit_flipper;
+    //	drive	the	display	through	a	7-seg	decoder
+    bcd7seg	digit_0	(bcd,	HEX0);
+endmodule
